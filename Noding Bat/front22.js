eeed6a022c844f1e21edcb58b5765c88cbd36e45
dkrusch <daniel.krusch@gmail.com>
2016-05-18 15:35:58 +0000

    finished 2 nodingbats and started on dewalt pg 4

---
+++ Noding Bat/front22.js
@@ -0,0 +1,9 @@
+// Given a string, take the first 2 chars and return the string with the 2
+// chars added at both the // front and back, so 'kitten' yields 'kikittenki'
+// chars are there.
+
+var front22=function(str) 
+{ 
+  	var twoChar = str.substring(0, 2);
+	return (twoChar + str + twoChar);
+}
---
+++ Noding Bat/in1020.js
@@ -0,0 +1,10 @@
+// Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+var isBetween10And20 =function(value)
+{
+    return (value >= 10 && value <= 20);
+}
+
+var in1020=function(a, b) 
+{ 
+	return (isBetween10And20(a) || isBetween10And20(b)) 
+}
