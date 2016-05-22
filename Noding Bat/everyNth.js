8e95f927c153512e1df7fe31c5d184e1119eef84
dkrusch <daniel.krusch@gmail.com>
2016-05-22 14:06:10 +0000

    added everyNth and monkeyTrouble, couldn't figure out everyNth

---
+++ Noding Bat/everyNth.js
@@ -0,0 +1,11 @@
+// Given a non-empty string and an int N, return the string made starting with char 0,
+// and then every Nth char of the string. So if N is 3, use char 0, 3, 6, ... and so on.
+// N is 1 or more.
+
+var everyNth=function(str, n)
+{ 
+	for (var i = 0; i < str.length; i++)
+    {
+      return str[i];
+    }
+}
