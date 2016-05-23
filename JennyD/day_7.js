d3485bee1fafa94e5a3d0057c907d2a100e7d0d1
dkrusch <daniel.krusch@gmail.com>
2016-05-23 15:11:59 +0000

    completed JD day 7, colored boxes

--- JennyD/day_7.js
+++ JennyD/day_7.js
@@ -1,8 +1,28 @@
+// In this application, when the mouse enters a box the boxes color will change
+
+var selectedBox = "box_1";
+
+// Creates a random color for the box
+function randomColor(event) 
+{
+    return '#' + Math.random().toString(16).slice(2, 8);
+};
+
+// When the mouse enters this function defines the box's id and changes applies a random color
+var selectBox = function(event)
+{
+    selectedBox = event.srcElement.id;
+	document.getElementById(selectedBox).style.backgroundColor = randomColor();
+};
+
+// When the window loads completely this function creates event listeners for the id's
+window.onload = function() 
+{
     var main = document.getElementById("main");
     console.log(main.children)
     for (i = 0; i < main.children.length; i++)
     {
         main.children[i].addEventListener("mouseenter",
-            paintBox);
+            selectBox, randomColor);
     }
 };
